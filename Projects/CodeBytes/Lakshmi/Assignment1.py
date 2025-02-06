7b909ce3ec935be9188406bf259ebc5e5f4c0308
kamallearner123 <kamalbec2004@gmail.com>
2025-02-06 15:55:01 +0530

    Added assignments on 3sum, zigzag

--- Projects/CodeBytes/Lakshmi/Assignment1.py
+++ Projects/CodeBytes/Lakshmi/Assignment1.py
@@ -35,7 +35,6 @@ Example 4:
 
     Output: true'''
 
-<<<<<<< Updated upstream
 def Solution(expression):
     # Stack to keep track of opening brackets
     stack = []
@@ -60,16 +59,3 @@ def Solution(expression):
 if __name__ == "__main__":
     assert(Solution(expression = "()") == True)
     assert(Solution(expression = "()[]{}") == True)
-=======
-class c1:
-    pass
-
-Version = "1.1: Date: 09/09/2021"
-def Solution(expression):
-    print("Solution funciton is getting called with expression = ", expression)
-
-print("Mudule name = ", __name__)
-if __name__ == "__main__": # If Assignment1.py is run directly, then only below code will run
-    Solution(expression = "()") == True
-    Solution(expression = "()[]{}") == True
->>>>>>> Stashed changes
